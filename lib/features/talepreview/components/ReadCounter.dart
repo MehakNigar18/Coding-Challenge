36360fec6a959e5196affc56e8dbcbdf24baa909
MehakNigar18 <127245431+MehakNigar18@users.noreply.github.com>
2023-06-11 12:28:32 +0200

    DONE

---
+++ lib/features/talepreview/components/ReadCounter.dart
@@ -0,0 +1,51 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ReadCounter extends StatefulWidget {
+  final int counter;
+  const ReadCounter({required this.counter, super.key});
+
+  @override
+  State<StatefulWidget> createState() => ReadCounterState();
+}
+
+class ReadCounterState extends State<ReadCounter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 700.w,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: 450.w,
+            child: Text(
+              'counter'.tr(args: [
+                widget.counter.toString(),
+              ]),
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    fontFamily: 'OpenDyslexic',
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w700,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+              textScaleFactor: 3.0,
+            ),
+          ),
+          widget.counter == 0
+              ? const Icon(
+                  Icons.check_box_outline_blank_outlined,
+                  size: 30,
+                )
+              : const Icon(
+                  Icons.check_box,
+                  color: Colors.green,
+                  size: 30,
+                ),
+        ],
+      ),
+    );
+  }
+}
